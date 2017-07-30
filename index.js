6518b5d50ab1f14db451d829c343c2f2aeee618f
Sami Ruokamo <sami.ruokamo@iki.fi>
2017-07-30 17:42:49 +0300

    Fix url

--- index.js
+++ index.js
@@ -17,33 +17,3 @@ app.get('/volume', function(req, res) {
 app.listen(3000, function() {
   console.log('OSX Remote Control listening on port 3000!');
 });
-/*
-// Load HTTP module
-var http = require("http");
-
-// Create HTTP server and listen on port 8000 for requests
-http.createServer(function(request, response) {
-
-   // Set the response HTTP header with HTTP status and Content type
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-
-   // Send the response body "Hello World"
-   response.end('Hello World\n');
-}).listen(8000);
-
-// Print URL for accessing server
-console.log('Server running at http://127.0.0.1:8000/');
-*/
-
-
-/*
-
-// note the eval method after require ¬
-var osascript = require('osascript').eval;
-
-// Run JavaScript text through OSA
-osascript('console.log("Hello, world!");', function (err, data) {
-  console.log(err, data);
-});
-
-*/
